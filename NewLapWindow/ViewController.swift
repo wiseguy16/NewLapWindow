e28184c6a4928efc9500f0817e05f41c572c6748
wiseguy16 <gergusa04@gmail.com>
2016-08-24 17:33:02 -0400

    Initial Commit

---
+++ NewLapWindow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewLapWindow
+//
+//  Created by Gregory Weiss on 8/24/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
